55ddad6738d0d65628f4018a0a42ab57f92d44e0
nihilok <mjfullstack@gmail.com>
2025-01-14 23:21:06 +0000

    Use body instead of query param

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from fastapi import FastAPI, Depends, HTTPException
 from fast_auth import settings, fast_auth, logged_in_user, User
+from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
 from lib.url_shortener import URLShortener
@@ -16,9 +17,13 @@ settings.cors_origins = os.getenv("CORS_ORIGINS", "*").split(",")
 fast_auth(app)
 
 
+class UrlBody(BaseModel):
+    url: str
+
+
 @app.post("/")
-async def shorten(url: str, user: User = Depends(logged_in_user)):
-    return {"url": await URLShortener.shorten(url)}
+async def shorten(body: UrlBody, user: User = Depends(logged_in_user)):
+    return {"url": await URLShortener.shorten(body.url)}
 
 
 @app.get("/{key}/")
